090635f0f898cb27c7a1ece43ab2379f56706365
david zhang <david_zhangxp@yahoo.com>
2024-11-03 20:10:34 -0500

    update20241103

--- src/app/ticket-list/ticket-list.component.ts
+++ src/app/ticket-list/ticket-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { tickets_list } from '../tickets';
 import { CommonModule } from '@angular/common';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-ticket-list',
@@ -13,6 +14,35 @@ export class TicketListComponent {
 
 
    tickets = tickets_list
+
+   excelData: any[] | undefined;
+   fileName = 'Breakfix_report.xlsx'
+ 
+   onFileChange(event: any) {
+     const file = event.target.files[0];
+     const reader = new FileReader();
+     reader.onload = (e: any) => {
+       const workbook = XLSX.read(e.target.result, { type: 'binary' });
+       const firstSheetName = workbook.SheetNames[0];
+       const worksheet = workbook.Sheets[firstSheetName];
+       this.excelData = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+       console.log('Excel data:', this.excelData);
+     };
+     reader.readAsBinaryString(file);
+   }
+ 
+   exportexcel(){
+     /* pass here the table id */
+     let element = document.getElementById('excel-table');
+     const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+  
+     /* generate workbook and add the worksheet */
+     const wb: XLSX.WorkBook = XLSX.utils.book_new();
+     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  
+     /* save to file */  
+     XLSX.writeFile(wb, this.fileName);
+   }
 
 
 }
